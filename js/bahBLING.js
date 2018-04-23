74cfbcaa72c2cd973868ae58a873b2212deb8809
Jason Burns <jasonb315@protonmail.com>
2018-04-22 21:12:35 -0700

    checkbox and radio done

--- js/bahBLING.js
+++ js/bahBLING.js
@@ -8,10 +8,44 @@ style_rules.push("#" + "pageSheet" + " { width:600px; margin: 0 auto; font-size:
 style_rules.push("." + "text" + " { color:#403C3D; font-size: 1.5em; } ");
 style_rules.push("." + "textLabel" + " { height: 40px; } ");
 
+style_rules.push("#" + "checkBox" + " { width: 110px; height: 150px; background-color: red; position: relative; overflow: hidden; } ");
+style_rules.push("#" + "checked" + " { display:block; height: 400px; position: absolute; z-index: 0; left: -233px;  top: -120px;;  } ");
+style_rules.push("#" + "unchecked" + " { display:block; height: 400px; position: absolute; z-index: 1; left: -95px;  top: -120px;  } ");
 
-
-
+style_rules.push("#" + "radioBox" + " { width: 110px; height: 150px; background-color: red; position: relative; overflow: hidden; } ");
+style_rules.push("#" + "booped" + " { display:block; height: 400px; position: absolute; z-index: 0; left: -503px;  top: -120px;;  } ");
+style_rules.push("#" + "unbooped" + " { display:block; height: 400px; position: absolute; z-index: 1; left: -360px;  top: -120px;  } ");
 
 
 var style = '<style type="text/css">' + style_rules.join("\n") + "</style>";
 $("head").append(style);
+
+
+$(document).ready(() => {
+
+  $('#unchecked').show();
+  $('#checked').hide();
+
+  $('#unchecked').on('click', () => {
+    $('#unchecked').hide();
+    $('#checked').show();
+  })
+  $('#checked').on('click', () => {
+    $('#checked').hide();
+    $('#unchecked').show();
+  })
+
+
+  $('#unbooped').show();
+  $('#booped').hide();
+
+  $('#unbooped').on('click', () => {
+    $('#unbooped').hide();
+    $('#booped').show();
+  })
+  $('#booped').on('click', () => {
+    $('#booped').hide();
+    $('#unbooped').show();
+  })
+
+})
